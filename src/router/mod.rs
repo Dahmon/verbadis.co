639f47e9acc7aa140a7117da0742c3e13855de9c
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-01-27 20:03:17 +1100

    feat: Add words page

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,10 +1,32 @@
+use std::sync::Arc;
+
 use askama_axum::Template;
-use axum::{response::Html, routing::get, Form};
+use axum::{
+    response::Html,
+    routing::{get, post},
+    Form,
+};
 use serde::Deserialize;
+
+use crate::AppState;
 
 #[derive(Template)]
 #[template(path = "root.html")]
 struct RootTemplate {}
+
+struct Word<'a> {
+    id: u32,
+    word: &'a str,
+    class: &'a str,
+    definition: &'a str,
+    example: Option<&'a str>,
+}
+
+#[derive(Template)]
+#[template(path = "pages/words.html")]
+struct WordsTemplate<'a> {
+    words: Vec<Word<'a>>,
+}
 
 #[derive(Template)]
 #[template(path = "train.html")]
@@ -16,15 +38,32 @@ struct TrainSubmission {
     definition: String,
 }
 
-pub fn app_router() -> axum::Router {
+pub fn app_router(state: Arc<AppState>) -> axum::Router {
     axum::Router::new()
         .route("/", get(root))
+        .route("/words", get(get_words))
+        .route("/words/new", post(post_words_new))
         .route("/train", get(get_train).post(post_train))
+        .with_state(state)
 }
 
 async fn root() -> RootTemplate {
     RootTemplate {}
 }
+
+async fn get_words<'a>() -> WordsTemplate<'a> {
+    let words = vec![Word {
+        id: 0,
+        word: "Hello",
+        class: "injunction",
+        definition: "A greeting",
+        example: None,
+    }];
+
+    WordsTemplate { words }
+}
+
+async fn post_words_new() {}
 
 async fn get_train() -> TrainTemplate {
     TrainTemplate {}
