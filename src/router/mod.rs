dab9ebb231b89d861d2b178e5f9c93d167981f03
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-01-27 21:55:59 +1100

    feat: Add create word form

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -2,11 +2,13 @@ use std::sync::Arc;
 
 use askama_axum::Template;
 use axum::{
-    response::Html,
+    extract::State,
+    response::Redirect,
     routing::{get, post},
     Form,
 };
 use serde::Deserialize;
+use sqlx::prelude::FromRow;
 
 use crate::AppState;
 
@@ -14,28 +16,27 @@ use crate::AppState;
 #[template(path = "root.html")]
 struct RootTemplate {}
 
-struct Word<'a> {
+#[derive(FromRow)]
+struct Word {
     id: u32,
-    word: &'a str,
-    class: &'a str,
-    definition: &'a str,
-    example: Option<&'a str>,
+    word: String,
+    class: String,
+    definition: String,
+    example: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct NewWordForm {
+    word: String,
+    class: String,
+    definition: String,
+    example: String,
 }
 
 #[derive(Template)]
 #[template(path = "pages/words.html")]
-struct WordsTemplate<'a> {
-    words: Vec<Word<'a>>,
-}
-
-#[derive(Template)]
-#[template(path = "train.html")]
-struct TrainTemplate {}
-
-#[derive(Deserialize)]
-struct TrainSubmission {
-    word: String,
-    definition: String,
+struct WordsTemplate {
+    words: Vec<Word>,
 }
 
 pub fn app_router(state: Arc<AppState>) -> axum::Router {
@@ -43,7 +44,6 @@ pub fn app_router(state: Arc<AppState>) -> axum::Router {
         .route("/", get(root))
         .route("/words", get(get_words))
         .route("/words/new", post(post_words_new))
-        .route("/train", get(get_train).post(post_train))
         .with_state(state)
 }
 
@@ -51,27 +51,30 @@ async fn root() -> RootTemplate {
     RootTemplate {}
 }
 
-async fn get_words<'a>() -> WordsTemplate<'a> {
-    let words = vec![Word {
-        id: 0,
-        word: "Hello",
-        class: "injunction",
-        definition: "A greeting",
-        example: None,
-    }];
+async fn get_words(State(state): State<Arc<AppState>>) -> WordsTemplate {
+    let words: Vec<Word> = sqlx::query_as("SELECT * FROM words")
+        .fetch_all(&state.pool)
+        .await
+        .expect("Failed to get words");
 
     WordsTemplate { words }
 }
 
-async fn post_words_new() {}
+async fn post_words_new(
+    State(state): State<Arc<AppState>>,
+    Form(form): Form<NewWordForm>,
+) -> Redirect {
+    sqlx::query("INSERT INTO words (word, class, definition, example) VALUES ($1, $2, $3, $4);")
+        .bind(form.word)
+        .bind(form.class)
+        .bind(form.definition)
+        .bind(form.example)
+        .execute(&state.pool)
+        .await
+        .expect("Failed to insert new word");
 
-async fn get_train() -> TrainTemplate {
-    TrainTemplate {}
-}
+    // TODO: Flash success/error message on session
 
-async fn post_train(Form(training): Form<TrainSubmission>) -> Html<String> {
-    Html(format!(
-        "<div>Word: {}, Definition: {}</div>",
-        training.word, training.definition
-    ))
+    // TODO: Replace #words-list with fragment
+    Redirect::to("/words")
 }
