0875e828b0eff5ea9a31c9cfbfef8a5de75b94c2
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-01-30 23:13:11 +1100

    feat: Add partial rendering to word search

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,9 +1,10 @@
+mod words;
+
 use std::sync::Arc;
 
-use askama_axum::Template;
+use askama_axum::{IntoResponse, Template};
 use axum::{
-    extract::State,
-    response::Redirect,
+    extract::{FromRequest, Request, State},
     routing::{get, post},
     Form,
 };
@@ -14,7 +15,7 @@ use crate::AppState;
 
 #[derive(Template)]
 #[template(path = "root.html")]
-struct RootTemplate {}
+struct RootPageTemplate {}
 
 #[derive(FromRow)]
 struct Word {
@@ -33,37 +34,98 @@ struct NewWordForm {
     example: String,
 }
 
+#[derive(Deserialize)]
+struct SearchForm {
+    search: Option<String>,
+}
+
 #[derive(Template)]
 #[template(path = "pages/words.html")]
-struct WordsTemplate {
+struct WordsPageTemplate {
     words: Vec<Word>,
+    search: String,
+}
+
+#[derive(Template)]
+#[template(path = "components/word-list.html")]
+struct WordsListTemplate {
+    words: Vec<Word>,
+}
+
+enum WordsTemplates {
+    Page(WordsPageTemplate),
+    List(WordsListTemplate),
+}
+
+// Even though both values implement IntoResponse, need to manually re-implement here.
+impl IntoResponse for WordsTemplates {
+    fn into_response(self) -> askama_axum::Response {
+        match self {
+            WordsTemplates::List(a) => a.into_response(),
+            WordsTemplates::Page(b) => b.into_response(),
+        }
+    }
 }
 
 pub fn app_router(state: Arc<AppState>) -> axum::Router {
     axum::Router::new()
         .route("/", get(root))
-        .route("/words", get(get_words))
+        .route("/words", get(get_words).post(post_words))
         .route("/words/new", post(post_words_new))
         .with_state(state)
 }
 
-async fn root() -> RootTemplate {
-    RootTemplate {}
+async fn root() -> RootPageTemplate {
+    RootPageTemplate {}
 }
 
-async fn get_words(State(state): State<Arc<AppState>>) -> WordsTemplate {
-    let words: Vec<Word> = sqlx::query_as("SELECT * FROM words")
+#[axum::debug_handler]
+async fn get_words(State(state): State<Arc<AppState>>, req: Request) -> WordsTemplates {
+    let is_search = req
+        .headers()
+        .get("HX-Target")
+        .and_then(|value| value.to_str().ok())
+        == Some("#words-list");
+
+    let form: Form<SearchForm> = Form::from_request(req, &state.clone()).await.unwrap();
+    let search: String = match &form.search {
+        Some(search) => search.clone(),
+        None => String::new(),
+    };
+
+    let words: Vec<Word> = sqlx::query_as("SELECT * FROM words WHERE word LIKE '%' || $1 || '%'")
+        .bind(&search)
         .fetch_all(&state.pool)
         .await
         .expect("Failed to get words");
 
-    WordsTemplate { words }
+    if is_search {
+        WordsTemplates::List(WordsListTemplate { words })
+    } else {
+        WordsTemplates::Page(WordsPageTemplate {
+            words,
+            search: search.clone(),
+        })
+    }
+}
+
+async fn post_words(
+    State(state): State<Arc<AppState>>,
+    Form(form): Form<SearchForm>,
+) -> WordsListTemplate {
+    let words: Vec<Word> = sqlx::query_as("SELECT * FROM words WHERE word LIKE '%' || $1 || '%'")
+        .bind(form.search)
+        .fetch_all(&state.pool)
+        .await
+        .expect("Failed to get words");
+
+    WordsListTemplate { words }
 }
 
 async fn post_words_new(
     State(state): State<Arc<AppState>>,
     Form(form): Form<NewWordForm>,
-) -> Redirect {
+) -> WordsListTemplate {
     sqlx::query("INSERT INTO words (word, class, definition, example) VALUES ($1, $2, $3, $4);")
         .bind(form.word)
         .bind(form.class)
@@ -75,6 +137,10 @@ async fn post_words_new(
 
     // TODO: Flash success/error message on session
 
-    // TODO: Replace #words-list with fragment
-    Redirect::to("/words")
+    let words: Vec<Word> = sqlx::query_as("SELECT * FROM words")
+        .fetch_all(&state.pool)
+        .await
+        .expect("Failed to get words");
+
+    WordsListTemplate { words }
 }
