4789c5340f7f8a26732b4a4866119531b38f38d6
Dahmon Bicheno <dahmon.bicheno@gmail.com>
2025-02-03 08:28:38 +1100

    feat: Serve css file

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,10 +1,15 @@
 mod challenges;
 mod words;
 
-use askama_axum::Template;
-use axum::routing::get;
+use askama_axum::{IntoResponse, Template};
+use axum::{
+    body::Body,
+    http::{Request, Response, StatusCode, Uri},
+    routing::get,
+};
 use challenges::challenges_router;
 use std::sync::Arc;
+use tower_http::services::ServeFile;
 
 use words::words_router;
 
@@ -17,6 +22,7 @@ struct RootPageTemplate {}
 pub fn app_router(state: Arc<AppState>) -> axum::Router {
     axum::Router::new()
         .route("/", get(root))
+        .route("/styles/styles.css", get(get_styles))
         .merge(words_router())
         .merge(challenges_router())
         .with_state(state)
@@ -24,4 +30,15 @@ pub fn app_router(state: Arc<AppState>) -> axum::Router {
 
 async fn root() -> RootPageTemplate {
     RootPageTemplate {}
+}
+
+async fn get_styles(uri: Uri) -> Result<Response<Body>, (StatusCode, String)> {
+    let req = Request::builder().uri(uri).body(Body::empty()).unwrap();
+    match ServeFile::new("styles/styles.css").try_call(req).await {
+        Ok(res) => Ok(res.into_response()),
+        Err(err) => Err((
+            StatusCode::NOT_FOUND,
+            format!("Unable to find file: {}", err),
+        )),
+    }
 }
